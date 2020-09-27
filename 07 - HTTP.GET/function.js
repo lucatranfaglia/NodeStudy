312448e625f4b223466483015e7bc7ef6ce49b6c
Luca Tranfaglia <lucatranfaglia@Lucky.local>
2020-09-27 16:55:50 +0200

    07 - HTTP-Get

---
+++ 07 - HTTP.GET/function.js
@@ -0,0 +1,31 @@
+function processResponse(res) {
+    res.pipe(write);
+    // visualizza la callback in console
+    res.pipe(process.stdout);
+
+    let response = "";
+    // chunk è un pezzo dello stream
+    // TRASMISSIONE PARZIALE
+    res.on('data', chunk => {
+        console.log("Stream chunk: ", chunk);
+        response += chunk;
+    });
+    // TRASMISSIONE COMPLETATA
+    res.on('end', () => {
+        try {
+            // converto il file da stringa ad oggetto (per poterci lavorare)
+            const weatherObj = JSON.parse(response);
+            console.log("weatherObj:", weatherObj);
+            console.log("weather:", weatherObj.weather[0].description);
+        } catch (e) {
+            console.error(e);
+        }
+    })
+}
+
+function errorResponse(status, error) {
+    console.error("Errore. Status: ", status, " - ", error);
+}
+
+exports.processResponse = processResponse;
+exports.errorResponse = errorResponse;
---
+++ 07 - HTTP.GET/http.get.js
@@ -0,0 +1,35 @@
+const https = require('https');
+const fs = require('fs');
+
+const {
+    url,
+    STATUS_CODE
+} = require('./costant');
+
+const {
+    processResponse,
+    errorResponse
+} = require('./function');
+
+
+const write = fs.createWriteStream(__dirname + '/weather.json');
+
+
+try {
+    // typeof result = STREAM
+    const req = https.get(url, (res) => {
+        const status = res.statusCode;
+        const error = STATUS_CODE[status];
+        switch (status) {
+            case 200:
+                processResponse(res);
+                break;
+            default:
+                errorResponse(status, error);
+                break;
+        }
+    });
+} catch (e) {
+    console.error("Error: ", e.message);
+
+}
